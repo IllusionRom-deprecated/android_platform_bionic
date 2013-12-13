8894301f5e323a7b40acfe4a4122d68ccfc2b7d8
Johnny Qiu <joqiu@nvidia.com>
2013-12-12 17:01:34 -0800

    libm: fast and precise sin()/cos()
    Rewrite upstream sin/cos with assembly. Jump paths are optimized.
    Introduce a PRECISE_TRIGONOMETRIC macro to select precision and
    performance. With PRECISE_TRIGONOMETRIC set, the error is within 1 ULP.
    The error is 2ULP if not set.
    
    Bug 1426401
    
    Change-Id: Iac712c0e6eb9bf42c87db9d7840c8aa3293ef674
    Reviewed-on: http://git-master/r/345073
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Shu Zhang <chazhang@nvidia.com>
    Tested-by: Shu Zhang <chazhang@nvidia.com>
    
    Conflicts:
    	libm/Android.mk

--- libm/arm/arm.mk
+++ libm/arm/arm.mk
@@ -7,6 +7,10 @@ libm_arch_src_files_arm := \
     arm/e_fast_exp.S \
     arm/e_sqrt.S \
     arm/e_sqrtf.S \
-    arm/s_fast_sin.S \
-    arm/s_fast_cos.S
+    arm/e_rem_pio2_fast.S \
+    arm/e_rem_pio2_large.c \
+    arm/k_sin_fast.S \
+    arm/k_cos_fast.S \
+    arm/s_sin_fast.S \
+    arm/s_cos_fast.S
 
