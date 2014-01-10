d5fb48b3d36ab42a54afde8ed2dae03ab4c1c60f
synergydev <synergye@codefi.re>
2014-01-09 22:30:37 -0800

    [ARM] libc: optimized strnlen, strncat, strncmp, and strlcat
    Summary:
    Date:   Mon Sep 23 14:45:06 2013 -0700
    
        [ARM] libc: use basic ASM strnlen implementation
    
        Summary:
        This is based on the current naive strlen routine from
        netbsd
    
        Test Plan: significant implemented bench gains
    
        Reviewers: randall.rushing, synergy
    
        Reviewed By: synergy
    
        Differential Revision: http://review.codefirex.com/D499
    
        Conflicts:
            libc/Android.mk
    
    Author: synergydev <synergye@codefi.re>
    Date:   Mon Sep 23 14:43:27 2013 -0700
    
        [ARM] libc: use optimized ASM strncmp implementation
    
        Summary:
        This strncmp implementation is based on the current
        arm netbsd implementation
    
        Test Plan: https://sourceware.org/git/?p=glibc.git;a=blob;f=benchtests/bench-strncmp.c;h=25df3dbc2eb25346d9a897f8d7a2ada9725584e3;hb=master and integrated bionic-benchmark
    
        Reviewers: randall.rushing
    
    Author: synergydev <synergye@codefi.re>
    Date:   Sat Sep 21 23:45:58 2013 -0700
    
        [ARM] libc: use basic ASM strncat implementation
    
        Summary:
        This implementation is the naive strncat implementation
        for ARM from netbsd upstream.
    
        Test Plan: Someone else test please, bench gain is significant with http://repo.or.cz/w/glibc.git/blob_plain/ec901dcf2886cfb81d88f9a63cfcd62b9efe6381:/benchtests/bench-strncat.c (no currently integrated bench, only test which is good with this change).
    
        Reviewers: randall.rushing, ConnorBaker, synergy
    
        Reviewed By: synergy
    
    Author: synergydev <synergye@codefi.re>
    Date:   Mon Sep 23 14:42:47 2013 -0700
    
        [ARM] libc: use basic ASM strlcat implementation
    
        Summary: This is based on the netbsd naive strlcat implementation
    
        Test Plan: +1 here
    
        Reviewers: randall.rushing, synergy
    
        Reviewed By: synergy
    
    Test Plan: significant implemented bench gains
    
    Reviewers: randall.rushing, synergy
    
    Reviewed By: synergy
    
    Differential Revision: http://review.codefirex.com/D238
    
    Conflicts:
    	libc/Android.mk
    
    Change-Id: I021fcba75f452d5f2fc43311ac1ad63e4e87c223

--- libc/Android.mk
+++ libc/Android.mk
@@ -366,16 +366,12 @@ ifeq ($(TARGET_ARCH),arm)
 libc_common_src_files += \
 	bionic/memmove.c.arm \
 	string/bcopy.c \
-	string/strncmp.c \
-	string/strncat.c \
 	string/strncpy.c \
 	bionic/strchr.cpp \
 	string/strrchr.c \
 	bionic/memchr.c \
 	bionic/memrchr.c \
 	string/index.c \
-	bionic/strnlen.c \
-	string/strlcat.c \
 	string/strlcpy.c \
 	upstream-freebsd/lib/libc/string/wcschr.c \
 	upstream-freebsd/lib/libc/string/wcsrchr.c \
