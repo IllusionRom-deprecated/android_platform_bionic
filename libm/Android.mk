spsforks/android-bionic-libc

libm: Optimize exp()
Perform fast exp(x) when |x| < 16.0.

Bug 1362653

Change-Id: I26060c75e7be63af169f00005fdb7cfe162c3a23
Reviewed-on: http://git-master/r/278360
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Johnny Qiu <joqiu@nvidia.com>
Tested-by: Johnny Qiu <joqiu@nvidia.com>
Reviewed-on: http://git-master/r/336759
Reviewed-by: Shu Zhang <chazhang@nvidia.com>
Tested-by: Shu Zhang <chazhang@nvidia.com>

libm/Android.mk
@@ -260,7 +260,9 @@ ifeq ($(TARGET_CPU_VARIANT),cortex-a9)
 libm_arm_src_files += \
     arm/k_log2.S \
     arm/k_pow2.S \
+    arm/k_exp.S \
     arm/e_fast_pow.S \
+    arm/e_fast_exp.S \
     arm/e_sqrt.S \
     arm/e_sqrtf.S \
     arm/s_floor.S
@@ -271,7 +273,9 @@ ifeq ($(TARGET_CPU_VARIANT),cortex-a15)
 libm_arm_src_files += \
     arm/k_log2.S \
     arm/k_pow2.S \
+    arm/k_exp.S \
     arm/e_fast_pow.S \
+    arm/e_fast_exp.S \
     arm/e_sqrt.S \
     arm/e_sqrtf.S \
     arm/s_floor.S \

IllusionRom-deprecated/android_platform_bionic

Conflicts:
	libm/Android.mk

libm/Android.mk
@@ -239,7 +239,9 @@ ifeq ($(TARGET_CPU_VARIANT),cortex-a9)
 libm_arm_src_files += \
     arm/k_log2.S \
     arm/k_pow2.S \
-    arm/e_fast_pow.S
+    arm/k_exp.S \
+    arm/e_fast_pow.S \
+    arm/e_fast_exp.S
 libm_arm_cflags += -DTARGET_CPU_VARIANT_CORTEX_A9
 libm_arm_asflags += -DTARGET_CPU_VARIANT_CORTEX_A9
 endif
@@ -247,7 +249,9 @@ ifeq ($(TARGET_CPU_VARIANT),cortex-a15)
 libm_arm_src_files += \
     arm/k_log2.S \
     arm/k_pow2.S \
-    arm/e_fast_pow.S
+    arm/k_exp.S \
+    arm/e_fast_pow.S \
+    arm/e_fast_exp.S
 libm_arm_cflags += -DTARGET_CPU_VARIANT_CORTEX_A15
 libm_arm_asflags += -DTARGET_CPU_VARIANT_CORTEX_A15
 endif
@@ -256,7 +260,9 @@ ifeq ($(TARGET_CPU_VARIANT),krait)
 libm_arm_src_files += \
     arm/k_log2.S \
     arm/k_pow2.S \
-    arm/e_fast_pow.S
+    arm/k_exp.S \
+    arm/e_fast_pow.S \
+    arm/e_fast_exp.S
 libm_arm_cflags += -DTARGET_CPU_VARIANT_CORTEX_KRAIT
 libm_arm_asflags += -DTARGET_CPU_VARIANT_CORTEX_KRAIT
 endif